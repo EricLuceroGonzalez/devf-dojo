8eb210b94bbad8bd98a14547472c3a75a59361e6
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-06-28 20:40:34 -0500

    node class request

--- js/04-devf-peticiones.js
+++ js/04-devf-peticiones.js
@@ -1,2 +1,29 @@
-// #
+// import { request } from "https";
+const request = require("request");
+
+// # Peticiones
 // Installing node and npm
+// init npm (npm init)
+// npm i request (package)
+console.log("Corriendo desde NodeJs...");
+
+// Use Request method:
+// const request = require("request");
+// request.get("http://www.google.com", function(error, response, body) {
+// console.error("error:", error); // Print the error if one occurred
+//   console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+//   console.log("body:", body); // Print the HTML for the Google homepage.
+//   console.log(response.statusCode);
+//   console.log(response.statusCode);
+// });
+
+// Using Star Wars API
+const swapi_url = "https://swapi.co/api/planets/1";
+
+request.get(swapi_url, (err, res, body) => {
+  if (res.statusCode === 200) {
+    console.log(body);
+  } else {
+    console.log(err && res);
+  }
+});
