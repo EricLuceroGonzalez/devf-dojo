96d1bd311677b77acaa9fd5ac33589559c146044
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-04 20:23:48 -0500

    excerics

--- js/07-API/07-APIs-ejercicios.js
+++ js/07-API/07-APIs-ejercicios.js
@@ -40,17 +40,9 @@ app.get("/api/v1/articulo", (req, res) => {
   res.status(200).send({ message: "todo cool!" });
 });
 
-// 3.- Agrega un endpoint '/api/usuario/' que responda a una petición de tipo GET con el nombre que sea
-//     recibido a través de params. El servidor debe responder con un código de estado 200 y un json como este:
-//                   {'usuario': 'Edwin'}
 
-// 4.- Agrega un endpoint '/api/swapi' que responda a una petición de tipo GET con el personaje solicitado de
-//                     https://swapi.co/
-//     El cliente debe mandar el número de personaje mediante params. La respuesta del servidor debe lucir algo así
-//                 {'personaje': {
-//                     'name': 'Luke Skywalker',
-//                     ...,
-//                 }}
+
+
 
 app.listen(port, () =>
   console.log(`Example app-exercise listening on port ${port}!!`)
--- js/07-API/07-js-API.js
+++ js/07-API/07-js-API.js
@@ -13,44 +13,83 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-// When some one calls .get() we catch the request data and send a response data
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+// 1.- Agrega un endpoint '/api/' que responda a una petición de tipo GET con un código de estado 200
+//     y el siguiente json:  {'mensaje':'hola mundo'}
+app.get("/api/", (req, res) => {
+  res.status(200).send({ message: "Hola mundo!!" });
 });
 
-app.get("/api/v1/prueba", (req, res) => {
-  res.status(200).send({ message: "ok" });
+// 2.- Agrega un endpoint '/api/suma' que responda a una petición de tipo GET con la suma de dos números que
+//     reciba mediante las querys num1 y num2. El servidor debe responder con un código de estado 200 y un json
+//     como el siguiente:    {'resultado': 7}
+app.get("/api/suma/", (req, res) => {
+  theSum = Number(req.query.num1) + Number(req.query.num2);
+  console.log("suma: " + theSum);
+  res.status(200).send({ message: "SUMA!!" });
 });
 
-app.delete("/api/v1/prueba", (req, res) => {
-  res.status(333).send({ message: "Hiciste post!" });
-});
-
-// When someone calls post() do this:
-app.post("/api/v1/usuario", (req, res) => {
-  console.log(req.body);
-  res.status(201).send({ message: "user created!", user: req.body });
-});
-
-// Another test get
-app.get("/api/v1/articulo", (req, res) => {
-  console.log(req.query);
-  console.log(req.query);
-  res.status(200).send({ message: "todo cool!" });
-});
-
-// Now with param
-app.get("/api/v1/articulo/:id/", (req, res) => {
+// 3.- Agrega un endpoint '/api/usuario/' que responda a una petición de tipo GET con el nombre que sea
+//     recibido a través de params. El servidor debe responder con un código de estado 200 y un json como este:
+//                   {'usuario': 'Edwin'}
+app.get("/api/:usuario", (req, res) => {
+  // console.log(req.body);
   console.log(req.params);
-  console.log(req.query);
-  res.status(200).send({ message: "todo cool! -- (id1)" });
+  res.status(200).send(req.params );
 });
 
-// Now with query
-app.get("/api/v1/articulo/", (req, res) => {
+// 4.- Agrega un endpoint '/api/swapi' que responda a una petición de tipo GET con el personaje solicitado de
+//                     https://swapi.co/
+//     El cliente debe mandar el número de personaje mediante params. La respuesta del servidor debe lucir algo así
+//                 {'personaje': {
+//                     'name': 'Luke Skywalker',
+//                     ...,
+//                 }}
+app.get("/api/swapi", (req, res) => {
+  // console.log(req.body);
   console.log(req.params);
-  console.log(req.query);
-  res.status(200).send({ message: "todo cool! -- (id2)" });
+  res.status(200).send(req.params );
 });
+
+
+
+// // When some one calls .get() we catch the request data and send a response data
+// app.get("/", (req, res) => {
+//   res.send("Hello World!!!");
+// });
+
+// app.get("/api/v1/prueba", (req, res) => {
+//   res.status(200).send({ message: "ok" });
+// });
+
+// app.delete("/api/v1/prueba", (req, res) => {
+//   res.status(333).send({ message: "Hiciste post!" });
+// });
+
+// // When someone calls post() do this:
+// app.post("/api/v1/usuario", (req, res) => {
+//   console.log(req.body);
+//   res.status(201).send({ message: "user created!", user: req.body });
+// });
+
+// // Another test get
+// app.get("/api/v1/articulo", (req, res) => {
+//   console.log(req.query);
+//   console.log(req.query);
+//   res.status(200).send({ message: "todo cool!" });
+// });
+
+// // Now with param
+// app.get("/api/v1/articulo/:id/", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.status(200).send({ message: "todo cool! -- (id1)" });
+// });
+
+// // Now with query
+// app.get("/api/v1/articulo/", (req, res) => {
+//   console.log(req.params);
+//   console.log(req.query);
+//   res.status(200).send({ message: "todo cool! -- (id2)" });
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!!`));
