10215356c99e6f76002343d267edea3f51bfd284
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-04 19:17:16 -0500

    git vs checking

--- js/07-API/07-js-API.js
+++ js/07-API/07-js-API.js
@@ -8,6 +8,7 @@ const port = 3000;
 // To parse a boydy to json
 var bodyParser = require("body-parser");
 
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
