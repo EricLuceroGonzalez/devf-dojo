565b34873ab85aaafbfa2ddca466afabe233306d
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-05 20:10:37 -0500

    set mongoose and cluster

---
+++ js/musica-API/database/index.js
@@ -0,0 +1,14 @@
+// Create varible that ...
+
+
+// ... manage Mongoose
+const mongoose = require("mongoose");
+
+// ... manage URL of mongo-atlas db (from cluster)  
+const db_uri = 'mongodb+srv://ericlucerogonzalez:lokikolo20*#mongodbatlas@cluster0-j4waz.mongodb.net/test?retryWrites=true&w=majority'
+
+// connect that mongoose with Mongo cluster
+mongoose.connect(db_uri, () => {
+    console.log('Conexion exitosa!');
+    
+})
