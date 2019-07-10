b0b998469f7a1f35ea20807118846c7ceefd2b3c
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-07-09 20:59:18 -0500

    all Set

--- js/musica-API/models/ArtistSchema.js
+++ js/musica-API/models/ArtistSchema.js
@@ -8,13 +8,19 @@ const artistSchema = new Schema({
   name: { type: String, required: true },
   genre: { type: String },
   integrants: [{ type: String }],
-  albums: [{ type: String }],
+  albums: [
+    {
+      name: { type: String },
+      coverPage: { type: String },
+      releaseDate: { type: Date }
+    }
+  ],
   originDate: { type: Date },
   is_active: { type: Boolean, default: true }
 });
 
 // Lets create (convert) this schema Model with ---> mongoose.model(modelName, schema):
-const ArtistSchema = mongoose.model('ArtistSchema', artistSchema);
+const ArtistSchema = mongoose.model("ArtistSchema", artistSchema);
 
 // Send it:
 module.exports = ArtistSchema;
